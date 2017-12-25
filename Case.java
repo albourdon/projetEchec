675032f0b2e2b3f269bf7db769c167f26898390d
alain <alain.bourd@gmail.com>
2017-12-25 12:31:54 +0100

    Classe de gestion d'une case de l'échiquier

---
+++ Case.java
@@ -0,0 +1,54 @@
+/**
+ * Classe regroupant les information d'une case de l'échiquier
+ * @author ALAIN BOURDON
+ *
+ */
+public class Case {
+
+
+	char abscisse;
+
+	int ordonnee;
+
+
+	public Case() {
+	}
+	/**
+	 * 
+	 * @param abscisse
+	 * @param ordonnee
+	 */
+	public Case(char abscisse,int ordonnee ) {
+		this.abscisse=abscisse;
+		this.ordonnee=ordonnee;
+	}
+	/**
+	 * 
+	 * 
+	 */
+	public char getAbscisse() {
+		return abscisse;
+	}
+	/**
+	 * 
+	 * @param abscisse
+	 */
+	public void setAbscisse(char abscisse) {
+		this.abscisse = abscisse;
+	}
+	/**
+	 * 
+	 * 
+	 */
+	public int getOrdonnee() {
+		return ordonnee;
+	}
+	/**
+	 * 
+	 * @param ordonnee
+	 */
+	public void setOrdonnee(int ordonnee) {
+		this.ordonnee = ordonnee;
+	}
+
+}
